92d61b323c3f5756fb742be9594722cf43af5b21
Diakahril <dhia.kahri@lobsterware.com>
2022-03-04 20:53:59 +0100

    top nav updated

--- src/app/components/top-nav/top-nav.component.ts
+++ src/app/components/top-nav/top-nav.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GameService} from "../../services/game.service";
+import {Category} from "../../models/category";
+import {Genre} from "../../models/genre";
+import {Platform} from "../../models/platform";
 
 @Component({
   selector: 'app-top-nav',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNavComponent implements OnInit {
 
-  constructor() { }
+  categories: Array<Category> = new Array<Category>();
+  genres: Array<Genre> = new Array<Genre>();
+  platforms: Array<Platform> = new Array<Platform>();
+
+  constructor(private gameService: GameService) {
+  }
 
   ngOnInit(): void {
+
+    this.gameService.getCategories().subscribe(res => {
+      this.categories = res;
+    })
+    this.gameService.getGenres().subscribe(res => {
+      this.genres = res;
+    })
+    this.gameService.getPlatforms().subscribe(res => {
+      this.platforms = res;
+    })
   }
 
 }
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -3,6 +3,9 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Game} from "../models/game";
 import {environment} from "../../environments/environment";
+import {Category} from "../models/category";
+import {Genre} from "../models/genre";
+import {Platform} from "../models/platform";
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +19,24 @@ export class GameService {
   getRecentlyAddedGames(): Observable<Array<Game>> {
     return this.http.get<Array<Game>>(`${environment.url}/games/added`)
   }
+
   getRecentlyReleasedGames(): Observable<Array<Game>> {
     return this.http.get<Array<Game>>(`${environment.url}/games/released`)
   }
-  getGameById(id:number): Observable<Game> {
+
+  getGameById(id: number): Observable<Game> {
     return this.http.get<Game>(`${environment.url}/games/${id}`)
+  }
+
+  getCategories(): Observable<Array<Category>> {
+    return this.http.get<Array<Category>>(`${environment.url}/categories`)
+  }
+
+  getGenres(): Observable<Array<Genre>> {
+    return this.http.get<Array<Genre>>(`${environment.url}/genres`)
+  }
+
+  getPlatforms(): Observable<Array<Platform>> {
+    return this.http.get<Array<Platform>>(`${environment.url}/platforms`)
   }
 }
