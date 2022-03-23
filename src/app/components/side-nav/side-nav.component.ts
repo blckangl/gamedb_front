ddbfdcae5cae803238b22ccf2698d2732ce6e7b3
dhia <dhiaa.kahri@gmail.com>
2022-03-23 20:08:41 +0100

    side menu

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  show: boolean = true;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleMenu() {
+    this.show = !this.show;
   }
-
 }
